0c67ccc55a968475734bc8c50279c6dc8bc96056
Jose1178 <wamiti8711@gmail.com>
2023-09-28 18:58:33 +0300

    Feat: usecontext

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
+   
+
   return (
     <div>
         <h3>History</h3>
