3f72b42c5bcd18975b4d645ed5e3a64957353690
Jose1178 <wamiti8711@gmail.com>
2023-09-28 19:12:01 +0300

    Feat: usecontext

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,21 +1,18 @@
-import React, {useContext} from 'react'
-import { GlobalContext } from '../context/GlobalState'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-    const { transactions } = useContext(GlobalContext);
-
-   
+  const { transactions } = useContext(GlobalContext);
 
   return (
-    <div>
-        <h3>History</h3>
-           <ul id='list' className="list">
-              <li className='minus'>
-            Cash <span>-$400</span><button className='delete-btn'>X</button>
-               </li>
-            </ul>   
-    </div>
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
   )
 }
-
 
